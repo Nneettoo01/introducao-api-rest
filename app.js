082d73c3efd9b5a78e8be3769c302c77ba9e9756
Neto <yasuoxd1v9@gmail.com>
2025-04-22 14:41:51 -0300

    feat: code status

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 import express from "express";
-const app = express();
-const port = 3000;
 
+const app = express();
+
+//  ==== Permitir ler JSON no corpo da requisição ====
 app.use(express.json());
 
+const port = 3000;
+
+// ==== Banco de dados Fake (em memória) ====
 const usuarios = [
   { id: 1, nome: "João", email: "joao@email.com" },
   { id: 2, nome: "Ana", email: "ana@email.com" },
 ];
 
-// Rota inicio
+// ==== Rota início ====
 app.get("/", (req, res) => {
-  res.send("Bem vindo a minha API");
+  res.send("Bem vindo a minha API!");
 });
 
-// Rota cria usuario
+// ==== Rota cria usuário ====
 app.post("/criarUsuario", (req, res) => {
   const { nome, email } = req.body;
 
-  // Adicionar o usuario no banco de dados fake(lista)
   const novoUsuario = {
     id: usuarios[usuarios.length - 1].id + 1,
     nome: nome,
     email: email,
   };
+
   usuarios.push(novoUsuario);
 
-  res.send(usuarios);
+  res.status(201).send(usuarios);
 });
 
+// ==== Atualizar usuário ====
 app.put("/usuario/:id", (req, res) => {
   const { id } = req.params;
   const { novoNome, novoEmail } = req.body;
@@ -36,29 +41,42 @@ app.put("/usuario/:id", (req, res) => {
   const indice = usuarios.findIndex((usuario) => {
     return usuario.id == id;
   });
+
+  //if indice === -1, dê como resposta da requisição
+  // o status 404
+  if (indice === -1) {
+    return res.status(404).json({ mensagem: "Usuário não encontrado!" });
+  }
+
   usuarios[indice].nome = novoNome;
   usuarios[indice].email = novoEmail;
 
   res.send(usuarios);
 });
 
-// Deletar usuario
-app.delete("/usuario/:id", (req, res) => {
+// ===== Exibir todos os usuários ====
+app.get("/usuarios", (req, res) => {
+  // res.json(usuarios)
+  res.status(200).json(usuarios);
+});
+
+// ==== Deletar usuário ====
+app.delete("/usuarios/:id", (req, res) => {
+  //const id = req.params.id
   const { id } = req.params;
 
   const index = usuarios.findIndex((usuario) => {
-    return usuario.id === id;
+    return usuario.id == parseInt(id);
   });
 
   if (index === -1) {
-    res.send("Usuario nao encontrado!");
+    res.status(404).json({ mensagem: "Usuário não encontrado!" });
+  } else {
+    usuarios.splice(index, 1);
+    res.send(usuarios);
   }
-
-  usuarios.splice(index, 1);
-
-  res.send(usuarios);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App escutando na  porta ${port}`);
 });
